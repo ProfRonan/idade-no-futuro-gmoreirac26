7d40e13ea8857ccbd8830fab1051efe934317172
Gabriel Moreira C <128189786+gmoreirac26@users.noreply.github.com>
2023-04-12 17:26:09 -0300

    atividade feita,final...talvez?
    Signed-off-by: Gabriel Moreira C <128189786+gmoreirac26@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ outro_ano = int(input('digite um ano próximo, para saber a sua idade nesse ano:
 nome = (input('por ultimo, digite seu nome: '))
 idade_outro_ano = idade_atual + (outro_ano - ano_atual)
 
-print(f'{nome},no ano de {outro_ano} você terá {idade_outro_ano} anos')
+print(f'{nome}, no ano de {outro_ano} você terá {idade_outro_ano} anos')
